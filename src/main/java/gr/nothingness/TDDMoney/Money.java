5acb2a9e3a329ecb354c0503b78402a0c49518ab
Christos Tsagkournis <Christos.Tsagkournis@sgdigital.com>
2020-04-05 16:30:26 +0300

    Refactoring

---
+++ src/main/java/gr/nothingness/TDDMoney/Money.java
@@ -0,0 +1,13 @@
+package gr.nothingness.TDDMoney;
+
+public class Money {
+
+  protected int amount;
+
+  @Override
+  public boolean equals(Object object) {
+    Money money = (Money) object;
+    return amount == money.amount && getClass().equals(money.getClass());
+  }
+
+}
--- src/test/java/gr/nothingness/TDDMoney/MoneyTest.java
+++ src/test/java/gr/nothingness/TDDMoney/MoneyTest.java
@@ -1,24 +1,50 @@
 package gr.nothingness.TDDMoney;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class MoneyTest {
 
+  //TODO: 5$ + 10CHF = 10$ if rate is 2:1
+  //DONE: 5$ * 2 = 10$
+  //DONE: Dollar side effects
+  //DONE: make amount private
+  //TODO: money rounding?
+  //DONE: equals()
+  //TODO: hashCode()
+  //TODO: equals() null
+  //TODO: equals() other objects
+  //DONE: 5CHF * 2 = 10CHF
+  //TODO: Dollar/Franc duplication
+  //DONE: common equals()
+  //TODO: common times()
+  //DONE: compare Dollars to Francs
+  //TODO: Currency?
+
   @Test
-  public void testMultiplication() {
+  public void testDollarMultiplication() {
     Dollar five = new Dollar(5);
-    Dollar product = five.times(2);
-    assertEquals(10, product.amount);
-    product = five.times(3);
-    assertEquals(15, product.amount);
+    assertEquals(new Dollar(10), five.times(2));
+    assertEquals(new Dollar(15), five.times(3));
+  }
+
+  @Test
+  public void testFrancMultiplication() {
+    Franc five = new Franc(5);
+    assertEquals(new Franc(10), five.times(2));
+    assertEquals(new Franc(15), five.times(3));
   }
 
   @Test
   public void testEquality() {
     assertTrue(new Dollar(5).equals(new Dollar(5)));
+    assertFalse(new Dollar(5).equals(new Dollar(6)));
+    assertTrue(new Franc(5).equals(new Franc(5)));
+    assertFalse(new Franc(5).equals(new Franc(6)));
+    assertFalse(new Franc(5).equals((new Dollar(5))));
   }
 
 }
